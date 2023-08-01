b100c8b943b9972b32854385a80a9d17c5e24068
Paris Nicolas <d47474935@alumnos.itr.edu.ar>
2023-08-01 19:07:14 -0300

    desafio 1

--- desafio_1/codigo2.js
+++ desafio_1/codigo2.js
@@ -1,23 +1,38 @@
 let primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]; //, 73, 79, 83, 89, 97];
 
+//Variable de debug para saber que multiplos encontro 
+let multiplos = [];
 
 
+//Funcion recursiva para encontrar el Multiplo mas grande utilizando los numeros primos
 function cadenas(a){
+    multiplos.push(a);
+    //CASO BASE
     if(a==1){
         return 1;
     }
 
+    //CASO RECURSIVO
     let sum = a;
     for(let n = 0; n<primos.length; n++){ 
         if(a % primos[n] == 0){
-            //sum += a/primos[n];
+            //sum += a/primos[n]; --> no hace falta ya lo suma "let sum = a" dentro de la recursividad
             sum += cadenas(a/primos[n]);
             return sum
         }
     }
-    console.log("Mutiplo no encontrado")
+    console.log("CONSEGIENDO MAS NUMEROS PRIMOS:")
+    while(!(a % primos[primos.length-1] == 0)){
+        console.log("-"+otroPrimo());
+    }
+    console.log("MULTIPLO ENCONTRADO: "+primos[primos.length-1]);
+    sum += cadenas(a/primos[primos.length-1]);
+    return sum
 }
 
+
+//Funcion encargada de encontrar mas numeros primos si es necesario
+//Basicamente empieza donde termino el ultimo primo obtenido y divide el sigiente numero por todos los anteriores, si no es divisible, es primo.
 function otroPrimo(){
     let prim = true;
     for(let m = primos[primos.length-1]; m<1000000; m++){
@@ -27,12 +42,13 @@ function otroPrimo(){
             }
         }
         if(prim){
-            primos.push(m);<    z<
+            primos.push(m);
             return m
         } 
         prim=true;
     }
 }
 
-console.log(cadenas(60));
-console.log(otroPrimo());
+//RESULTADOS
+console.log("\n_" + cadenas(261997) + "_ Es el resultado");
+console.log(multiplos);
