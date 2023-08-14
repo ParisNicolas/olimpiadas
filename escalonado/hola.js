90807284227e892830161198c13f4fe24024e623
Paris Nicolas <d47474935@alumnos.itr.edu.ar>
2023-08-14 20:36:29 -0300

    ahah

--- escalonado/hola.js
+++ escalonado/hola.js
@@ -1,21 +1,54 @@
 let sum = 0;
             //4,3,2,1,0 posiciones
 //let digits = [3,4,2,8,2].reverse(); //numero 34.282
-let digits = [1,3,0].reverse();
+let digits = [1,9,0].reverse();
 
-let nulFunc = (a,b,c)=> a;
 
-function escalone(n){
-    if(n==0)
-    sumer(7, 0, sumer);
-}
+/*
+N=8 --> s = 1 mini
+N=7 --> s = 2 peque
+N=6 --> s = 3 ciclo
+N=5 --> s = 4 hiperciclc
 
-function sumer(n, R = 0, F = nulFunc, s){
-    if(n==1 || R>=1){
-        return F(n);
+R --> Limitante (default hasta 1)
+*/
+function sumer(n, s, R = 0){
+    //Casos Base
+    if(n==1 || s<1){
+        return n;
+    }else if(R==1){
+        return sumer(n, s-1);
     }
-    return F(n) + sumer(n-1, R-1, F);
+
+    //Casos recursivos
+    return sumer(n, s-1) + sumer(n-1, s, R-1);
 }
+
+/*
+let cicles = digits.map((dig, i)=>{
+    let d;
+    if(dig <= 9-i){
+        d = dig;
+    }else{
+        d = 9-i;
+    }
+    let rest = d-digits[i+1];
+
+    if(i === digits.length-1){
+        return d;
+    }
+    if(rest >=0 && i === 0){
+        return rest;
+    }
+    else if(rest > 1){
+        return rest-1;
+    }
+    else{
+        return 0;
+    }
+    
+});
+console.log(cicles);
 
 digits.map((d,i)=>{
     if(i == 0 && d>digits[i+1]){
@@ -33,5 +66,5 @@ digits.map((d,i)=>{
         }
     }
 });
-console.log(sum);
-//console.log(sumer(7, 0, sumer));
+console.log(sum);*/
+//console.log(sumer(7, 0, sumer));*/
