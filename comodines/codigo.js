e63d567cb06c827007300f78f03fb306ef77a542
Nicolas Francisco Paris <d47474935@instituto.itr.edu.ar>
2023-09-07 16:02:47 -0300

    comodines

--- comodines/codigo.js
+++ comodines/codigo.js
@@ -1,22 +1,36 @@
+/*
 let arr = [[2,4,5],
            [3,0,4],
            [2,4,5],
            [0,4,5],
            [1,1,4]];
+*/
 
-let visited = [[0,0,0],
-               [0,0,0],
-               [0,0,0],
-               [0,0,0],
-               [0,0,0]];
+let arr = [
+    [2, 4, 5, 3, 0, 4, 2, 4, 5, 0],
+    [0, 4, 5, 1, 1, 4, 3, 0, 4, 2],
+    [2, 4, 5, 0, 4, 5, 2, 4, 5, 0],
+    [0, 4, 5, 3, 0, 4, 2, 4, 5, 0],
+    [2, 4, 5, 1, 1, 4, 3, 0, 4, 2],
+    [2, 4, 5, 3, 0, 4, 2, 4, 5, 0],
+    [2, 4, 5, 3, 0, 4, 2, 4, 5, 0],
+    [2, 4, 5, 3, 0, 4, 2, 4, 5, 0],
+    [2, 4, 5, 3, 0, 4, 2, 4, 5, 0],
+    [2, 4, 5, 3, 0, 4, 2, 4, 5, 0]
+];
 
-let maxCardNum = [0, 0]; //num, block_size
+
+
+let visited,zeros = [];
+
+visited = arr.map(() => arr[0].map(() => 0));
 
 function expandVirus(x, y, cardNum){
-    if(x<0 || x>arr[0].length || y<0 || y>arr.length) return 0;
+    if(x<0 || x>=arr[0].length || y<0 || y>=arr.length) return 0;
     if(visited[y][x]) return 0;
     if(arr[y][x] != cardNum && arr[y][x] != 0) return 0;
 
+    if(arr[y][x] == 0) zeros.push([x, y]);
     visited[y][x] = true;
     let numOfCards = 1;
 
@@ -34,11 +48,15 @@ function comodines(){
     for(let y = 0; y < arr.length; y++){
         for(let x = 0; x < arr[0].length; x++){
             let num = arr[y][x];
-            let result = expandVirus(y, x, num);
+            let result = expandVirus(x, y, num);
             if(result > maxCardNum[0]){
                 maxCardNum[0]=result;
                 maxCardNum[1]=num;
             }
+            for (let i in zeros) {
+                visited[zeros[i][1]][zeros[i][0]] = false;
+            }
+            zeros = [];
         }
     }
     return maxCardNum;
